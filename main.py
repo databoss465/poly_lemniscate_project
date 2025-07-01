8054e5f9cddd9dc13368c2f4976214c6bbe67fc0
Your Name <dattabhasvan@iisc.ac.in>
2025-07-01 17:05:32 +0530

    multiprocessing for local_search

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ import time
 import json
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+
 from multiprocessing import Pool
+from tqdm import tqdm
+
 from utils import *
 from scoring import *
 from plotter import display_lemniscate
+
 
 # Prereq:
 # 0. Generate starter dataset
@@ -53,7 +58,7 @@ def local_search (root_positions: list[float], max_steps:int=100) -> list[comple
             improved = True
 
         elif count > max_steps:
-            print(f"Failed to improve after {count} iterations. Returning original positions.")
+            # print(f"Failed to improve after {count} iterations. Returning original positions.")
             break
               
     return new_positions, cddt_score, improved
@@ -67,43 +72,93 @@ def rep_local_search (root_positions: list[float], max_steps:int=100, reps:int=1
     new_positions = root_positions.copy()
     failed_attempts = 0
     for i in range(reps):
-        if i % display_status == 0:
-            print(f"Iteration {i+1}/{reps}")
+        # if i % display_status == 0:
+        #     print(f"Iteration {i+1}/{reps}")
         new_positions, new_score, improved = local_search(new_positions, max_steps=max_steps)
         
         if not improved:
             failed_attempts += 1
             if failed_attempts >= tolerance:
-                print(f"Failed to improve after {failed_attempts} consecutive attempts. Stopping local search.")
+                # print(f"Failed to improve after {failed_attempts} consecutive attempts. Stopping local search.")
                 break
         else:
             failed_attempts = 0
     
-    return new_positions, new_score
+    return canonical(new_positions), new_score
+
+# Worker function for parallel processing
+def populator (bs:int, deg: int, max_steps:int=100, reps:int=100, tolerance:int=10)  -> list[tuple[list[float], float]]:
+    """
+    Worker function for parallel processing.
+    Generates a random set of root positions and runs local search on them.
+    Returns a list of tuples containing the root positions and the score.
+    """
+    results = []
+    for _ in tqdm(range(bs), desc="Worker progress", position=0, leave=False):
+        root_positions = canonical(np.random.uniform(0, 1, deg).tolist())
+        new_positions, new_score = rep_local_search(root_positions, max_steps=max_steps, reps=reps, tolerance=tolerance)
+        results.append((new_positions, new_score))
+
+    return results
+
+def generate_population (pop_size:int, deg:int, max_steps:int=100, reps:int=100, tolerance:int=10, num_workers: int = 12) -> pd.DataFrame:
+    """
+    Generate a sorted population of canonical root positions using parallel processing.
+
+    Parameters:
+    - pop_size: Total number of root configs to generate
+    - deg: Degree of the polynomial
+    - max_steps: Maximum number of steps for local search
+    - reps: Number of repetitions of local search
+    - tolerance: Number of consecutive failed attempts before stopping local search
+    - num_workers: Number of parallel workers to use
+    """
+
+    bs = pop_size // num_workers
+    args = [(bs, deg, max_steps, reps, tolerance) for _ in range(num_workers)]
+
+    print(f"Generating population of {pop_size} root positions with degree {deg} using {num_workers} workers...")
+    with Pool(processes=num_workers) as pool:
+            all_batches = pool.starmap(populator, args)
+
+    # Flatten the list of lists
+    population = [item for batch in all_batches for item in batch]
+    df = pd.DataFrame(population, columns=['root_positions', 'score'])
+    df['root_positions'] = df['root_positions'].apply(lambda x: canonical(x))
+    df = df.sort_values(by='score', ascending=False).reset_index(drop=True)
+
+    return df
+    
         
 
 if __name__ == "__main__":
 
-    deg, reps = 20, 200
-    # root_positions = [k / n for k in range(n)]
-    root_positions = np.random.uniform(0, 1, deg).tolist()
-    root_positions = canonical(root_positions)
+    deg = 15
+    total_pop = 1000
+    max_steps = 100
+    reps = 100
+    tolerance = 10
 
+    filepath = "poly_lemniscate_project/Samples"
+    filename = f"population{total_pop}_deg{deg}.csv"
+    plotpath = "poly_lemniscate_project/Images"
+    plotname = f"population{total_pop}_deg{deg}.png"
+    
     t = time.time()
-    new_positions, new_score = rep_local_search(root_positions, max_steps=20, reps=reps)
+    population_df = generate_population(total_pop, deg, max_steps=max_steps, reps=reps, tolerance=tolerance)
+    population_df.to_csv(os.path.join(filepath, filename), index=False)
     t = time.time() - t
-    new_positions = canonical(new_positions)
-    # new_roots = root_generator_circle(new_positions)
-    # display_lemniscate(new_roots, count=69)
-    init_score, fin_score = score(root_positions), score(new_positions)
-    print(f"Initial score: {init_score:.4f}, Final score: {fin_score:.4f}, Time taken: {t:.2f}s")
+    print(f"Population generated and saved to {os.path.join(filepath, filename)} in {t:.2f}s.")
 
+    plt.hist(population_df['score'], bins=20, color='cyan', alpha=0.7, edgecolor='black')
+    plt.title(f"Score Distribution for Degree {deg} Population")
+    plt.xlabel("Score")
+    plt.ylabel("Frequency")
+    plt.grid(True)
+    plt.tight_layout()
+    plt.savefig(os.path.join(plotpath, plotname), dpi=300)
+    plt.close()
 
-    roots = root_generator_circle(root_positions)
-    display_lemniscate(roots, count=70)
-
-    new_roots = root_generator_circle(new_positions)
-    display_lemniscate(new_roots, count=71)
 
 
 
